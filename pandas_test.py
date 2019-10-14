6486bfdfe8a120ab56dbac00e3b60a52064761fa
Arvin Kent S. Lazaga <arvinkent121816@gmail.com>
2019-10-14 12:50:05 +0800

    * installed xlrd module for excel dependency * added an xlsx example file * added function to read excel files

--- pandas_test.py
+++ pandas_test.py
@@ -1,5 +1,19 @@
 import pandas as pd
 
+# Opens and reads a csv file
 df = pd.read_csv('biostats_test.csv')
 
+# Prints all data from the csv file
 print(df)
+
+# Prints the first 5 rows from the csv file
+print(df.head(5))
+
+# Opens and reads a xlsx file
+df_xlsx = pd.read_excel('people_test.xlsx')
+
+# Prints all data from the xlsx file
+print(df_xlsx)
+
+# Prints the first 5 rows from the csv file
+print(df_xlsx.head(5))
